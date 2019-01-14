5547f31687a9585511ce672a19c2fd8e39f432d6
Triavanicus <triavanicus@gmail.com>
2019-01-14 00:45:37 -0600

    Added Rendering and Update Loop
    So chip8 has two timers (sound and delay) that tick down at 60Hz and all of the
    updates happen at 1000Hz. So since I am using SFML I implemented the delays with
    the clock object, and created a RectangleShape that I will be using to draw the
    pixels with.

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,24 @@
 
 int main()
 {
+    const int windowWidth = 1280;
+    const int windowHeight = 720;
+    const int chip8Width = 64;
+    const int chip8Height = 32;
+
     sf::RenderWindow window = {};
-    window.create(sf::VideoMode(1280, 720), "Chip8 Interpreter");
+    window.create(
+        sf::VideoMode(windowWidth, windowHeight), "Chip8 Interpreter");
+
+    auto chip8 = Chip8();
+
+    auto chip8Clock = sf::Clock();
+    auto chip8TimerClock = sf::Clock();
+
+    auto pixelWidth = (float) windowWidth / chip8Width;
+    auto pixelHeight = (float) windowHeight / chip8Height;
+    sf::RectangleShape pixel = sf::RectangleShape(
+        sf::Vector2f(pixelWidth, pixelHeight));
 
     while (window.isOpen())
     {
@@ -15,8 +31,39 @@ int main()
             if (event.type == sf::Event::Closed) { window.close(); }
         }
 
-        window.clear();
-        window.display();
+        if (chip8Clock.getElapsedTime().asMicroseconds() >= 1000)
+        {
+            chip8.cycle();
+            chip8Clock.restart();
+        }
+
+        if (chip8TimerClock.getElapsedTime().asMilliseconds() >= 16)
+        {
+            if (chip8.soundTimer > 0) chip8.soundTimer--;
+            if (chip8.delayTimer > 0) chip8.delayTimer--;
+
+            chip8TimerClock.restart();
+        }
+
+        if (chip8.drawFlag)
+        {
+            window.clear();
+            {
+                for (auto y = 0; y < chip8Height; y++)
+                    for (auto x = 0; x < chip8Width; x++)
+                    {
+                        if (chip8.pixels[x + y * chip8Width] > 0)
+                        {
+                            pixel.setPosition(x * pixelWidth, y * pixelHeight);
+                            window.draw(pixel);
+                        }
+                    }
+            }
+            window.display();
+            chip8.drawFlag = false;
+        }
     }
+
+    delete[] data;
     return 0;
 }
