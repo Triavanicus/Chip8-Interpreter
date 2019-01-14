e6e09050903b51d350ff75d1d5b9d2e3e965d3fe
Triavanicus <triavanicus@gmail.com>
2019-01-13 21:48:58 -0600

    Create SFML Window
    So to kick things off, we are going to need a window to handle the key input and
    drawing of the Chip8 Interpreter.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Hello Chip8!" << std::endl;
+    sf::RenderWindow window = {};
+    window.create(sf::VideoMode(1280, 720), "Chip8 Interpreter");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) { window.close(); }
+        }
+
+        window.clear();
+        window.display();
+    }
     return 0;
 }
